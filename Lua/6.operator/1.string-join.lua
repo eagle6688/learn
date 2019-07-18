16fe307ed3832655b78264350de288324869de59
Aldwin Su <fangyansu@163.com>
2019-07-18 20:11:30 +0800

    Add two lua test cases.

---
+++ Lua/6.operator/1.string-join.lua
@@ -0,0 +1,7 @@
+local a = "Hello"
+local b = "World"
+print(a..b)
+print(a .. b)
+print(#a)
+print(#a .. b)
+print(#(a .. b))
---
+++ Lua/6.operator/2.math.lua
@@ -0,0 +1,10 @@
+a = 20
+b = 10
+c = 15
+d = 5
+
+local e = a + (b * c) / d;
+print("a + (b * c) / d:", e);
+
+e = a + b * c / d;
+print("a + b * c / d:", e);
